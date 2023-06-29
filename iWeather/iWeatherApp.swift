9598acc9e97109f34d8f19a6a6409684212cf741
Smit Patel <smit24@icloud.com>
2023-06-29 19:21:29 +0530

    Initial Commit

---
+++ iWeather/iWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWeatherApp.swift
+//  iWeather
+//
+//  Created by Smit Patel on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
