142f617ad520ed8e8b8c9d38e70939381b17aa44
Smit Patel <smit24@icloud.com>
2023-07-01 23:53:50 +0530

    added open weather api and getting latitude and longitutde

---
+++ iWeather/Managers/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  iWeather
+//
+//  Created by Smit Patel on 01/07/23.
+//
+
+import Foundation
---
+++ iWeather/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  iWeather
+//
+//  Created by Smit Patel on 29/06/23.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
