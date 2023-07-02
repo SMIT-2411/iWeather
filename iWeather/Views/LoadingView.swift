ad59f3087069c1a202bff7a60abc58d7d1d23c8a
Smit Patel <smit24@icloud.com>
2023-07-02 12:18:32 +0530

    completed the welcome view and api configuration

---
+++ iWeather/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  iWeather
+//
+//  Created by Smit Patel on 02/07/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
